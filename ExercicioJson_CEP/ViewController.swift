a0293c07dcde90461943d0b6dc8c92605a5f4f5a
Matheus <matheuspedro1509@gmail.com>
2017-09-18 15:32:21 -0300

    Exemplo Json

--- ExercicioJson_CEP/ViewController.swift
+++ ExercicioJson_CEP/ViewController.swift
@@ -10,6 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txvCep: UITextField!
+    @IBOutlet weak var txvRua: UITextField!
+    @IBOutlet var txvBairro: UITextField!
+    @IBOutlet weak var txvCidade: UITextField!
+    @IBOutlet weak var txvUf: UITextField!
+    
+    
+    
+    
+    
+    
+    var session: URLSession?
+    
+    @IBAction func buscar(_ sender: Any) {
+        
+        let config = URLSessionConfiguration.default
+        session = URLSession(configuration: config)
+        
+        let cep = "http://viacep.com.br/ws/" +  txvCep.text! + "/json/"
+        
+        let url = URL(string: cep)
+        
+        let task = session!.dataTask(with: url!){ (data, response, error) in
+            
+            let rua = self.retornarNomeRua(data: data!, campo: "logradouro")
+            let cidade = self.retornarNomeRua(data: data!, campo: "localidade")
+            let bairro = self.retornarNomeRua(data: data!, campo: "bairro")
+            let estado = self.retornarNomeRua(data: data!, campo: "uf")
+            DispatchQueue.main.async(execute: {
+                self.txvRua.text = rua
+                self.txvCidade.text = cidade
+                self.txvBairro.text = bairro
+                self.txvUf.text = estado
+        
+            })
+        }
+        task.resume()
+    }
+    
+    func retornarNomeRua(data: Data, campo:String) -> String?{
+        var resposta: String?=nil
+        do{
+            let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: AnyObject]
+            if let nomeRua = json[campo] as? String{
+                return nomeRua
+            }
+        }catch let error as NSError {
+            return "Falha ao carregar :\(error.localizedDescription)"
+        }
+        return resposta
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
